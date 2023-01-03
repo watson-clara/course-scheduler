883b37cf5ff4408696d2bb0f77c8c12615bbaa01
Clara Watson <watsoncl@oregonstate.edu>
2023-01-03 13:44:11 -0800

    fix

--- courses/scripts/scrape_soc.py
+++ courses/scripts/scrape_soc.py
@@ -42,7 +42,7 @@ def fetch_courses(is_verbose):
         total_rows = responses[-1]['TOTALROWS']
         next_row = responses[-1]['LASTROW']
         if (next_row is None) or (next_row > total_rows): next = True
-        print('') if next else break
+        print('') if next = True else break
     if is_verbose: print('Recieved course data from one.uf.edu')
     courses_nested_list = [req['COURSES'] for req in responses]
     return [course for sublist in courses_nested_list for course in sublist]
